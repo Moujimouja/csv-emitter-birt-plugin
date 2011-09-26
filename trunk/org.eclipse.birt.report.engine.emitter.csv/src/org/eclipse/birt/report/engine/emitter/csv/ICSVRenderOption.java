eada91b40bd9fedc954ac3d170b36b8ec09cc8de
mksethi2009@gmail.com <mksethi2009@gmail.com@dac6eb4d-f00b-ef46-8aaf-fdf56eff8b1f>
2011-09-26 06:44:50 +0000

    Added New Render Options

--- trunk/org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/ICSVRenderOption.java
+++ trunk/org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/ICSVRenderOption.java
@@ -3,9 +3,26 @@ package org.eclipse.birt.report.engine.emitter.csv;
 import org.eclipse.birt.report.engine.api.IRenderOption;
 
 public interface ICSVRenderOption extends IRenderOption{
-
-	public static final String OUTPUT_EMITTERID_CSV = "org.eclipse.birt.report.engine.emitter.csv";
-	public static final String OUTPUT_FORMAT_CSV = "csv";
-	public static final String OUTPUT_DATATYPE_IN_SECOND_ROW = "csvRenderOption.outputDatatypeInSecondRow";
 	
+	public static final String OUTPUT_EMITTERID_CSV = "org.eclipse.birt.report.engine.emitter.csv";	
+	
+	public static final String OUTPUT_FORMAT_CSV = "csv";
+	
+	/**
+	 * The option to decide to show data type in second row of output CSV
+	 */
+	public static final String SHOW_DATATYPE_IN_SECOND_ROW = "csvRenderOption.showDatatypeInSecondRow";
+	
+	/**
+	 * The option to export a specific table by name in the CSV Output
+	 */
+	public static final String EXPORT_TABLE_BY_NAME = "csvRenderOption.exportTableByName";
+	
+	public void setShowDatatypeInSecondRow(boolean showDatatypeInSecondRow);
+	
+	public boolean getShowDatatypeInSecondRow();
+	
+	public void setExportTableByName(String tableName);
+	
+	public String getExportTableByName();
 }
