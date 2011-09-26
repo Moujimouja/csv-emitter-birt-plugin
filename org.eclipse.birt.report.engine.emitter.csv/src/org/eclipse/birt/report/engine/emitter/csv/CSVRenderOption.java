403e0dc78a83445ff09910fe7df60b04b0d53afc
mksethi2009 <mksethi2009@gmail.com>
2011-09-26 06:46:53 +0000

    Added New Render Option 1.Option to Specify to output data type in second row 2.Option to Specify a table name to output

--- org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/CSVRenderOption.java
+++ org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/CSVRenderOption.java
@@ -21,4 +21,42 @@ public class CSVRenderOption extends RenderOption implements ICSVRenderOption{
 	{
 		super( options );
 	}
+
+	/**
+	 * Set show datatype in second row
+	 * 
+	 * @param showDatatypeInSecondRow
+	 */
+	public void setShowDatatypeInSecondRow(boolean showDatatypeInSecondRow)
+	{	
+		setOption( SHOW_DATATYPE_IN_SECOND_ROW, showDatatypeInSecondRow );		
+	}
+
+	/**
+	 * Get show datatype in second row
+	 * 
+	 * @param
+	 */
+	public boolean getShowDatatypeInSecondRow() {
+		return getBooleanOption(SHOW_DATATYPE_IN_SECOND_ROW, true);
+	}
+
+	/**
+	 * Set Export Table By Name option
+	 * 
+	 * @param tableName
+	 */
+	public void setExportTableByName(String tableName) {
+		setOption(EXPORT_TABLE_BY_NAME,tableName);
+		
+	}
+
+	/**
+	 * Get Export Table By Name option
+	 * 
+	 * @param
+	 */
+	public String getExportTableByName() {
+		return getStringOption(EXPORT_TABLE_BY_NAME);
+	}
 }
