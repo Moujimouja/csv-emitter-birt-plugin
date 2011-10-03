7e87376dc3a9a6bd88f291f38267a99e1b4b64b6
mksethi2009@gmail.com <mksethi2009@gmail.com@dac6eb4d-f00b-ef46-8aaf-fdf56eff8b1f>
2011-10-03 15:50:40 +0000

    Added New Render Options

--- trunk/org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/ICSVRenderOption.java
+++ trunk/org.eclipse.birt.report.engine.emitter.csv/src/org/eclipse/birt/report/engine/emitter/csv/ICSVRenderOption.java
@@ -4,8 +4,14 @@ import org.eclipse.birt.report.engine.api.IRenderOption;
 
 public interface ICSVRenderOption extends IRenderOption{
 	
+	/**
+	 * CSV Emitter Id
+	 */
 	public static final String OUTPUT_EMITTERID_CSV = "org.eclipse.birt.report.engine.emitter.csv";	
 	
+	/**
+	 * CSV Output Format
+	 */
 	public static final String OUTPUT_FORMAT_CSV = "csv";
 	
 	/**
@@ -18,6 +24,17 @@ public interface ICSVRenderOption extends IRenderOption{
 	 */
 	public static final String EXPORT_TABLE_BY_NAME = "csvRenderOption.exportTableByName";
 	
+	/**
+	 * The option to specify the field delimiter, default is comma
+	 */
+	public static final String DELIMITER = "csvRenderOption.Delimiter";
+	
+	/**
+	 * The option to specify the character with which delimiter should be replaced with 
+	 * if it occurs inside the text, default is blank space
+	 */
+	public static final String REPLACE_DELIMITER_INSIDE_TEXT_WITH = "csvRenderOption.replaceDelimiterInsideTextWith";
+	
 	public void setShowDatatypeInSecondRow(boolean showDatatypeInSecondRow);
 	
 	public boolean getShowDatatypeInSecondRow();
@@ -25,4 +42,12 @@ public interface ICSVRenderOption extends IRenderOption{
 	public void setExportTableByName(String tableName);
 	
 	public String getExportTableByName();
+	
+	public void setDelimiter (String fieldDelimiter);
+	
+	public String getDelimiter ();
+	
+	public void setReplaceDelimiterInsideTextWith (String replaceDelimiterInsideTextWith);
+	
+	public String getReplaceDelimiterInsideTextWith ();
 }
